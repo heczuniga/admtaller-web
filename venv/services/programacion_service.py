b15acbceca2a424f24639292c3af14856c517471
hector.zuniga.luarte@gmail.com <hector.zuniga.luarte@gmail.com>
2023-06-17 11:16:49 -0400

    Se implementa la eliminación de programación de asignaturas.

--- venv/services/programacion_service.py
+++ venv/services/programacion_service.py
@@ -28,3 +28,26 @@ async def get_programaciones_asignatura_lista(ano_academ: int, id_usuario: int) 
     return agrupadores
 
 
+async def delete_programacion_asignatura(request: Request, ano_academ: int, cod_periodo_academ: int, sigla: str, seccion: int) -> Optional[dict]:
+
+    # Recuperamos el usuario conectado desde la cookie para pasarlo a los servicios como parámetro para segmentar datos
+    id_usuario = cookie_autoriz.get_id_usuario_cookie(request)
+    error_message: str = None
+
+    # Armamos la URL de la API respectiva
+    url = f"{APITaller.URL_BASE.value}/programacion/eliminar/ano_academ/{ano_academ}/cod_periodo_academ/{cod_periodo_academ}/sigla/{sigla}/seccion/{seccion}/{id_usuario}"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response: Response = await client.delete(url)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as e:
+            error_message = e.response.json().get("detail")
+            if "409" not in str(e):
+                raise Exception(f"Error en la llamada a la API respectiva. [{error_message}]")
+        except httpx.RequestError as e:
+            raise Exception(f"Error de conexión con la API respectiva. [{str(e)}]")
+
+    # Si todo está correcto, Retornamos la respuesta de la API
+    eliminacion = response.json()
+    return eliminacion
--- venv/views/programacion.py
+++ venv/views/programacion.py
@@ -17,3 +17,21 @@ async def programaciones_asignatura_lista(request: Request):
     await vm.load()
 
     return vm.to_dict()
+
+
+@router.get("/programacion/eliminar/ano_academ/{ano_academ}/cod_periodo_academ/{cod_periodo_academ}/sigla/{sigla}/seccion/{seccion}")
+@template(template_file="programacion/programacion_asignatura_lista.pt")
+async def eliminar_asignatura(request: Request, ano_academ: int, cod_periodo_academ: int, sigla: str, seccion: int):
+    eliminacion = await programacion_service.delete_programacion_asignatura(request, ano_academ, cod_periodo_academ, sigla, seccion)
+
+    if not eliminacion["eliminado"]:
+        vm = ProgramacionesAsignaturaViewModel(request)
+        await vm.load()
+        vm.msg_error = eliminacion["msg_error"]
+        return vm.to_dict()
+
+    # Si no hay errores, se redirecciona a la página principal
+    response = fastapi.responses.RedirectResponse("/programacion/asignatura/lista", status_code=status.HTTP_302_FOUND)
+    return response
+
+
